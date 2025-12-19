5afb33e9dd3bd24b7c9f89993069e68058301ce0
caroline-2 <carolinehanna510@gmail.com>
2025-12-19 02:21:13 +0200

    Apply Requested Changes

--- backend/src/main/java/com/clashcode/backend/service/UserService.java
+++ backend/src/main/java/com/clashcode/backend/service/UserService.java
@@ -66,7 +66,7 @@ public class UserService {
         return 20;
     }
 
-    private String getRank(int rate) {
+    public String getRank(int rate) {
         int index = rate / 300;
         if (index >= Ranks.values().length)
             index = Ranks.values().length - 1;
@@ -152,7 +152,7 @@ public class UserService {
         }
     }
 
-    private String buildImageUrl(String fileName) {
+    public String buildImageUrl(String fileName) {
         if (fileName == null || fileName.isEmpty()) {
             return null;
         }
